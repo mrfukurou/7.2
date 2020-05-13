28ea18c9e1c8b494b3a74614b6621287822bfe9c
fukur <fukur@LAPTOP-7EQMGM6H>
2020-05-13 23:04:50 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите строку: ");
+                StringBuilder a = new StringBuilder(Console.ReadLine());
+                string str = a.ToString();
+                if (!String.IsNullOrWhiteSpace(str))
+                {
+                    Console.Write("Слова, которые встречаются один раз в предложении: ");
+                    var s = str.ToLower().Split(new[] { ' ','.', ',', '?', '!', ':', ';' }, StringSplitOptions.RemoveEmptyEntries)
+                    .GroupBy(x => x)
+                    .Where(x => x.Count() == 1)
+                    .Select(x => x.Key);
+                    foreach (var rez in s) Console.Write( rez + ",");
+                    Console.WriteLine();
+                }
+                else
+                {
+                throw new Exception();
+                }
+            }
+            catch { Console.WriteLine("Некорректный ввод! "); }
+        }
+
+	}
+}
+ 
